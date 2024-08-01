2ff08bdaa69475498ee80ecd265c84eee3101a6e
James-P-aul <119072034+James-P-aul@users.noreply.github.com>
2024-08-01 18:41:19 +0530

    Update connectToMongo.js

--- backend/config/connectToMongo.js
+++ backend/config/connectToMongo.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 const connectToMongo = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://joyjoypaul1:3yyCmCyuajHT7RrI@cluster0.xwuo6am.mongodb.net/Chat-App"
+      ""
     );
     console.log("Connected to MongoDB");
   } catch (error) {
